5194a50eb72b6c2e16e777c54441ab3e8bd36e14
ewan-keith <keith.ewan@yahoo.co.uk>
2019-06-30 22:03:50 +0100

    moved int tests up into top level directory for neatness

--- integration-tests/run-int-tests.sh
+++ integration-tests/run-int-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 image_name="open-aps-int-tests:latest"
 
-docker build -t $image_name integration-test-project/ && \
+docker build -t $image_name . && \
 	docker run --rm --network="openapsstreaming_test-network" $image_name
 
