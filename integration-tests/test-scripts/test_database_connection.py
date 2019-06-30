b3d7c0bd74c99975d8c995621c6f912c58e72bb9
ewan-keith <keith.ewan@yahoo.co.uk>
2019-06-30 23:02:19 +0100

    added database connection test (with hardcoded config)

--- integration-tests/test-scripts/test_database_connection.py
+++ integration-tests/test-scripts/test_database_connection.py
@@ -1,6 +1,19 @@
 """
 
 """
+import psycopg2
+
+
+
+def test_database_connection():
+    try:
+        conn = psycopg2.connect("host='open-aps-db' dbname='open_aps' user='int_test_admin' password='just_a_testing_password'")
+        conn_successful = True
+    except Exception as e:
+        print("Database connection failed: " + str(e))
+        conn_successful = False
+    assert conn_successful
+
 
 def func(x):
     return x + 1
