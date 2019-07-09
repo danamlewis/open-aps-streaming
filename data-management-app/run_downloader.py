209892d3989ad4456719b282863301c755c93824
Ewan Keith <ewan.keith@mudano.com>
2019-07-09 16:55:21 +0100

    fixed more merge conflicts

--- data-management-app/run_downloader.py
+++ data-management-app/run_downloader.py
@@ -2,8 +2,4 @@
 from downloader import app
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(host='0.0.0.0', port=9999)
-=======
-    app.run(port=9999)
->>>>>>> 9ab545046a0e36b4dd63eada84730801ecc5c481
