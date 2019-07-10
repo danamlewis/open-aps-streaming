91843f360f4b01761389829d6552e11034f2b0b1
Ewan Keith <ewan.keith@mudano.com>
2019-07-10 11:07:18 +0100

    added a reading of the users last ingest time from their openhumans account to the ingest job

--- nightscout-ingester/ingester/jobs.py
+++ nightscout-ingester/ingester/jobs.py
@@ -26,11 +26,13 @@ def nightscout_ingest_job():
 
         # pull the nightscout URL file from OH
         nightscout_url = user.fetch_ns_url()
+        last_recorded_at = user.fetch_last_recorded_at()
 
-        # any failure to fetch url will result in a None, handled below
-        if nightscout_url:
+        # any failure to fetch either url or last recorded files will result in no transfer, handled below
+        if nightscout_url and last_recorded_at:
             nightscout_site = NightscoutSite(nightscout_url)
             print(nightscout_site)
+            print(last_recorded_at)
 
             # pull the users nightscout data from this URL
 
