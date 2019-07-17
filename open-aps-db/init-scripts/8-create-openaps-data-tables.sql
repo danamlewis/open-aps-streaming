263915d87e7ddbe9b0e3211bb9637009d132d880
Laurie-Bamber <40243209+Laurie-Bamber@users.noreply.github.com>
2019-07-17 11:37:44 +0100

    Update 8-create-openaps-data-tables.sql

--- open-aps-db/init-scripts/8-create-openaps-data-tables.sql
+++ open-aps-db/init-scripts/8-create-openaps-data-tables.sql
@@ -1,109 +1,105 @@
 
 CREATE TABLE openaps.device_status (
-  seq_id BIGSERIAL PRIMARY KEY,
-  app_id INTEGER,
-  id VARCHAR,
-  device VARCHAR,
-  pump_id VARCHAR,
-  pump_bolusing BOOL,
-  pump_suspended BOOL,
-  pump_model VARCHAR,
-  loop_cob NUMERIC,
-  loop_iob NUMERIC,
-  loop_version VARCHAR,
-  loop_failure_reason TEXT,
-  snooze VARCHAR,
-  override_active BOOL,
-  raw_json JSONB,
-  created_at timestamp
+    seq_id BIGSERIAL PRIMARY KEY,
+    user_id INTEGER,
+	id VARCHAR,
+	device VARCHAR,
+	pump_id VARCHAR,
+	pump_bolusing BOOL,
+	pump_suspended BOOL,
+	pump_model VARCHAR,
+	loop_cob NUMERIC,
+	loop_iob NUMERIC,
+	loop_version VARCHAR,
+	loop_failure_reason TEXT,
+	snooze VARCHAR,
+	override_active BOOL,
+	source_entity INTEGER,
+	raw_json JSONB,
+	created_at timestamp
 );
-
 GRANT SELECT ON TABLE openaps.device_status TO viewer;
 GRANT SELECT ON TABLE openaps.device_status TO ext_openaps_app;
 GRANT SELECT, INSERT ON TABLE openaps.device_status TO ingestor;
 
 
 CREATE TABLE openaps.device_status_metrics (
-  seq_id BIGSERIAL PRIMARY KEY,
-  device_status_id VARCHAR,
-  iob_iob NUMERIC,
-  iob_activity NUMERIC,
-  iob_basal_iob NUMERIC,
-  iob_bolus_iob NUMERIC,
-  iob_net_basal_insulin NUMERIC,
-  iob_bolus_insulin NUMERIC,
-  iob_timestamp TIMESTAMP,
-  suggested_temp VARCHAR,
-  suggested_bg NUMERIC,
-  suggested_tick VARCHAR,
-  suggested_eventual_bg NUMERIC,
-  suggested_insulin_req NUMERIC,
-  suggested_reservoir NUMERIC,
-  suggested_cob NUMERIC,
-  suggested_iob NUMERIC,
-  enacted_temp VARCHAR,
-  enacted_bg NUMERIC,
-  enacted_tick VARCHAR,
-  enacted_eventual_bg NUMERIC,
-  enacted_insulin_req NUMERIC,
-  enacted_reservoir VARCHAR,
-  enacted_cob NUMERIC,
-  enacted_iob NUMERIC,
-  enacted_duration NUMERIC,
-  enacted_rate NUMERIC,
-  raw_json JSONB,
-  enacted_timestamp TIMESTAMP
+    seq_id BIGSERIAL PRIMARY KEY,
+    device_status_id VARCHAR,
+    iob_iob NUMERIC,
+    iob_activity NUMERIC,
+    iob_basal_iob NUMERIC,
+    iob_bolus_iob NUMERIC,
+    iob_net_basal_insulin NUMERIC,
+    iob_bolus_insulin NUMERIC,
+    iob_timestamp TIMESTAMP,
+    suggested_temp VARCHAR,
+    suggested_bg NUMERIC,
+    suggested_tick VARCHAR,
+    suggested_eventual_bg NUMERIC,
+    suggested_insulin_req NUMERIC,
+    suggested_reservoir NUMERIC,
+    suggested_cob NUMERIC,
+    suggested_iob NUMERIC,
+    enacted_temp VARCHAR,
+    enacted_bg NUMERIC,
+    enacted_tick VARCHAR,
+    enacted_eventual_bg NUMERIC,
+    enacted_insulin_req NUMERIC,
+    enacted_reservoir VARCHAR,
+    enacted_cob NUMERIC,
+    enacted_iob NUMERIC,
+    enacted_duration NUMERIC,
+    enacted_rate NUMERIC,
+    enacted_timestamp TIMESTAMP
 );
-
 GRANT SELECT ON TABLE openaps.device_status_metrics TO viewer;
 GRANT SELECT ON TABLE openaps.device_status_metrics TO ext_openaps_app;
 GRANT SELECT, INSERT ON TABLE openaps.device_status_metrics TO ingestor;
 
 
 CREATE TABLE openaps.entries (
-  seq_id BIGSERIAL PRIMARY KEY,
-  app_id INTEGER,
-  id VARCHAR,
-  sgv NUMERIC,
-  direction VARCHAR,
-  device VARCHAR,
-  type VARCHAR,
-  rssi NUMERIC,
-  rawbg NUMERIC,
-  trend VARCHAR,
-  glucose NUMERIC,
-  mbg NUMERIC,
-  delta NUMERIC,
-  filtered NUMERIC,
-  unfiltered NUMERIC,
-  noise NUMERIC,
-  "scale" NUMERIC,
-  slope NUMERIC,
-  intercept NUMERIC,
-  system_time VARCHAR,
-  raw_json JSONB,
-  "date" timestamp
+	seq_id BIGSERIAL PRIMARY KEY,
+    user_id INTEGER,
+	id VARCHAR,
+	sgv NUMERIC,
+	direction VARCHAR,
+	device VARCHAR,
+	type VARCHAR,
+	rssi NUMERIC,
+	rawbg NUMERIC,
+	trend VARCHAR,
+	glucose NUMERIC,
+	mbg NUMERIC,
+	delta NUMERIC,
+	filtered NUMERIC,
+	unfiltered NUMERIC,
+	noise NUMERIC,
+	"scale" NUMERIC,
+	slope NUMERIC,
+	intercept NUMERIC,
+	system_time VARCHAR,
+	source_entity INTEGER,
+	raw_json JSONB,
+	"date" timestamp
 );
-
 GRANT SELECT ON TABLE openaps.entries TO viewer;
 GRANT SELECT ON TABLE openaps.entries TO ext_openaps_app;
 GRANT SELECT, INSERT ON TABLE openaps.entries TO ingestor;
 
 
 CREATE TABLE openaps.profile (
-  seq_id BIGSERIAL PRIMARY KEY,
-  app_id INTEGER,
-  "id" VARCHAR,
-  "default_profile" text,
-  mills int8,
-  units VARCHAR,
-  store JSON,
-  loop_settings JSON,
-  start_date TIMESTAMP,
-  raw_json JSONB,
-  created_at timestamp
+	seq_id BIGSERIAL PRIMARY KEY,
+    user_id INTEGER,
+	"id" VARCHAR,
+	"default_profile" text,
+	mills int8,
+	units VARCHAR,
+	start_date TIMESTAMP,
+	source_entity INTEGER,
+	raw_json JSONB,
+	created_at timestamp
 );
-
 GRANT SELECT ON TABLE openaps.profile TO viewer;
 GRANT SELECT ON TABLE openaps.profile TO ext_openaps_app;
 GRANT SELECT, INSERT ON TABLE openaps.profile TO ingestor;
@@ -111,43 +107,40 @@ GRANT SELECT ON openaps.profile TO ext_openaps_app;
 
 
 CREATE TABLE openaps.treatments (
-  seq_id BIGSERIAL PRIMARY KEY,
-  app_id INTEGER,
-  "id" VARCHAR,
-  event_type VARCHAR,
-  timestamp TIMESTAMP,
-  insulin NUMERIC,
-  carbs NUMERIC,
-  protein NUMERIC,
-  fat NUMERIC,
-  glucose NUMERIC,
-  glucose_type VARCHAR,
-  food_type VARCHAR,
-  temp VARCHAR,
-  rate NUMERIC,
-  duration NUMERIC,
-  units VARCHAR,
-  amount NUMERIC,
-  absolute NUMERIC,
-  bolus JSON,
-  boluscalc JSON,
-  medtronic VARCHAR,
-  type VARCHAR,
-  absorption_time NUMERIC,
-  unabsorbed NUMERIC,
-  ratio NUMERIC,
-  wizard JSON,
-  target_top NUMERIC,
-  target_bottom NUMERIC,
-  fixed NUMERIC,
-  programmed VARCHAR,
-  reason VARCHAR,
-  notes TEXT,
-  entered_by VARCHAR,
-  raw_json JSONB,
-  created_at timestamp
+    seq_id BIGSERIAL PRIMARY KEY,
+    user_id INTEGER,
+	"id" VARCHAR,
+	event_type VARCHAR,
+	timestamp TIMESTAMP,
+	insulin NUMERIC,
+	carbs NUMERIC,
+	protein NUMERIC,
+	fat NUMERIC,
+	glucose NUMERIC,
+	glucose_type VARCHAR,
+	food_type VARCHAR,
+	temp VARCHAR,
+	rate NUMERIC,
+	duration NUMERIC,
+	units VARCHAR,
+	amount NUMERIC,
+	absolute NUMERIC,
+	medtronic VARCHAR,
+	type VARCHAR,
+	absorption_time NUMERIC,
+	unabsorbed NUMERIC,
+	ratio NUMERIC,
+	target_top NUMERIC,
+	target_bottom NUMERIC,
+	fixed NUMERIC,
+	programmed VARCHAR,
+	reason VARCHAR,
+	notes TEXT,
+	entered_by VARCHAR,
+	source_entity INTEGER,
+	raw_json JSONB,
+	created_at timestamp
 );
-
 GRANT SELECT ON TABLE openaps.treatments TO viewer;
 GRANT SELECT ON TABLE openaps.treatments TO ext_openaps_app;
 GRANT SELECT, INSERT ON TABLE openaps.treatments TO ingestor;
@@ -178,9 +171,26 @@ CREATE TABLE openaps.member_demographics (
   updated_ts TIMESTAMP,
   CONSTRAINT member_demographics_project_member_id_pkey UNIQUE (project_member_id, ts)
 );
-
 GRANT SELECT ON TABLE openaps.member_demographics TO viewer;
 GRANT SELECT ON TABLE openaps.member_demographics TO admin_viewer;
+
+
+CREATE TABLE openaps.source_entities (
+	
+	id BIGSERIAL PRIMARY KEY,
+	name VARCHAR,
+	inserted_ts TIMESTAMP
+	);
+ALTER TABLE openaps.source_entities OWNER TO power_user;
+GRANT SELECT, INSERT ON TABLE openaps.source_entities TO ingestor;
+GRANT SELECT ON TABLE openaps.source_entities TO viewer;
+GRANT SELECT ON TABLE openaps.source_entities TO admin_viewer;
+
+INSERT INTO openaps.source_entities
+(name, inserted_ts)
+values
+(1, 'OpenAPS Data Commons', CURRENT_TIMESTAMP),
+(2, 'NightScout Data Commons', CURRENT_TIMESTAMP);
 
 
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA openaps TO ingestor;
