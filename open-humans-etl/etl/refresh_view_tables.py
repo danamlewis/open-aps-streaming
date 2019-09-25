82ea22128a8f8ac6414958c46c42a112b28d74ca
LaurieBamber <43658144+LaurieBamber@users.noreply.github.com>
2019-09-25 12:12:47 +0100

    Adding source_entity column to view definitions

--- open-humans-etl/etl/refresh_view_tables.py
+++ open-humans-etl/etl/refresh_view_tables.py
@@ -110,7 +110,7 @@ def create_entries_table(pg_connection):
       to_timestamp('1900-01-01 ' || t2.end_tm,'YYYY-MM-DD HH24:MI:SS') as end_tmstamp,
       t1.date_exact, t1.event_type, t1.noise, t1.rssi, t1.rawbg, t1.trend, t1.glucose,
       t1.mbg, t1.delta, t1.filtered, t1.unfiltered, t1.direction, t1.scale, t1.slope,
-      t1.intercept, t1.sgv_unaltered, t1.sgv_mmol_L,
+      t1.intercept, t1.sgv_unaltered, t1.sgv_mmol_L, t1.source_entity,
       case when t3.loop_type is null then 'open loop' else t3.loop_type end as loop_type,
       t1.device,
       case
@@ -131,7 +131,7 @@ def create_entries_table(pg_connection):
         select
           user_id as "app_id", "id" as entry_id, "date" as entry_datetime, "date"::time as time_exact, "date"::date as date_exact,
           "type" || ' - entry' as event_type , device, noise, rssi, rawbg, trend, glucose, mbg, delta, filtered,
-          unfiltered, direction, scale, slope, intercept, sgv as sgv_unaltered,
+          unfiltered, direction, scale, slope, intercept, sgv as sgv_unaltered, source_entity,
           case 
             when sgv < 25 then sgv else sgv / 18
           end as sgv_mmol_L
