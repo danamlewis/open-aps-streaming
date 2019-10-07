704dc1ff40998208a678ad0cc61fbf88a8c2e04b
LaurieBamber <43658144+LaurieBamber@users.noreply.github.com>
2019-10-07 21:17:17 +0100

    Replacing helper with environ

--- data-management-app/downloader/func/analytics.py
+++ data-management-app/downloader/func/analytics.py
@@ -13,35 +13,39 @@ def retrieve_iframes():
 
             For each entity:
                 2. Get token using environ Metabase Secret Key
-                3. Construct URL from token and environ Metabase URL
+                3. Construct URL from token and environ Metabase URL - skip URL if user doesn't have access
 
         :returns: A dictionary containing dashboard URL's for Metabase
 
     """
-
-    from downloader.helpers import metabase_secret_key, metabase_url
-    METABASE_URL = metabase_url()
-    METABASE_KEY = metabase_secret_key()
 
     iframe_mapper = get_dashboard_mapper()
 
     for dashboard, params in iframe_mapper.items():
 
         if params['number'] == 'NA':
+
             params['url'] = 'none'
+
         else:
             payload = {
                 "resource": {"dashboard": params['number']},
                 "params": {
                 }
             }
-            token = jwt.encode(payload, METABASE_KEY, "HS256")
-            params['url'] = METABASE_URL + "/embed/dashboard/" + token.decode("utf8") + "#theme=night&bordered=false&titled=false"
+            token = jwt.encode(payload, os.environ['METABASE_SECRET_KEY'], "HS256")
+            params['url'] = os.environ['METABASE_URL'] + "/embed/dashboard/" + token.decode("utf8") + "#theme=night&bordered=false&titled=false"
 
     return iframe_mapper
 
 
 def get_dashboard_mapper():
+
+    """
+        This function returns the ID's of the dashboards that a user is allowed to see,
+        based on the allowed_projects field in the user table. Dashboards are duplicated,
+        and filtered by the Open Humans project that their data is sourced from.
+    """
 
     if current_user.allowed_projects == 0:
 
