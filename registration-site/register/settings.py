bb0553ec4f123a420c924811b58088a9f1757bd8
Ewan Keith <ewan.keith@mudano.com>
2019-10-22 09:44:28 +0100

    prepped Django app for production use

--- registration-site/register/settings.py
+++ registration-site/register/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r-j0z2&x-$m)y-rn$)vf0n3ib&188z(2d4k1*t5dyc&$clcw=5'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
