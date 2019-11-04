ac90beb397678a9d004f897e181169e0ff90b557
Ewan Keith <ewan.keith@mudano.com>
2019-11-04 17:21:13 +0000

    added a small adjustment to make django host its own static files in production mod

--- registration-site/register/urls.py
+++ registration-site/register/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import home, logout_view, transfer, deauth_view
+from django.views.static import serve 
 
 urlpatterns = [
     path(r'', home, name='home'),
@@ -23,6 +24,8 @@ urlpatterns = [
     path(r'transfer', transfer, name='transfer'),
     path('openhumans/', include('openhumans.urls')),
     path('deauth', deauth_view, name='deauth'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
